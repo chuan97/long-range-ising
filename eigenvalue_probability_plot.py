Fix calculation of D_k

eigenvalue_probability_plot.py
@@ -7,8 +7,8 @@ import plot
 import interactions
 
 def Dk_exact(Jrs, k, N):
-    rs = np.arange(0, N//2)
-    return np.sum(Jrs * np.cos(2 * np.pi * k * rs / N))
+    rs = np.arange(1, N//2)
+    return Jrs[0] + np.sum(Jrs[1:] * 2 * np.cos(k * rs))
 
 plot.set_rcParams(size = (10, 9), lw = 2, fs = 20)
 
@@ -30,8 +30,10 @@ for i, alpha in enumerate(alphas):
 
     vals = eigh(Jbase, eigvals_only=True)
     #if j == 0:
-    ax.plot(np.arange(N) / N, vals[::-1], c=colors[0], lw=0, marker='o') 
-    ax.plot(np.arange(N) / N, [Dk_exact(Jbase[:N//2, 0], k, N) for k in np.arange(N)])
+    ks = np.arange(0, N)
+    ax.plot(ks / N, vals[::-1], c=colors[0], lw=0, marker='o') 
+    Dks = [Dk_exact(Jbase[:N//2, 0], 2*np.pi*k/N, N) for k in ks]
+    ax.plot(ks / N, Dks)
     ax.axhline(0, lw=0.5, c='k')
     
     if i == 0:
21ac9767e70f1a8ce13682a78b177c83ce41f1f2
JuanR <jroman@unizar.es>
2023-05-17 17:54:18 +0200

    Update bS plot and antiferro tests

--- bS_plot_2D.py
+++ bS_plot_2D.py
@@ -11,55 +11,52 @@ ax = axes[0]
 
 cmap = plt.get_cmap('viridis')
 
-alphas = [0.1, 0.5, 1.0, 1.5, 2.0]
-Ns = np.arange(10, 100, 1)
+alphas = [0.5, 1, 2.0, 3.0, 4.0]
+Ns = np.arange(2, 20, 1)
 
 colors = cmap(np.linspace(0, 1, 10))
 for i, alpha in enumerate(alphas):
+    print(alpha)
+    Ss = np.array([np.sum(interactions.powerlaw_pbc_2D(N, alpha)[0]) for N in Ns])
     
-    Ss = np.array([np.sum(interactions.powerlaw_pbc(N, alpha)[0]) for N in Ns])
-    
-    c = colors[5] if alpha == 1.0 else 'k'
+    c = colors[5] if alpha == 2.0 else 'k'
     ax.plot(np.log10(Ns), Ss, label=alpha, c=c)
     
-    label = str(alpha)
-    xpos = np.log10(Ns)[10 * (i + 1)]
-    if i == 0:
-        label = r'$\alpha = $' + label
-        xpos = np.log10(Ns)[10 * (i + 1) - 4]
-    ax.text(xpos, Ss[10 * (i + 1)], label, fontsize=15, c=c, backgroundcolor='white')
+    # label = str(alpha)
+    # xpos = np.log10(Ns)[10 * (i + 1)]
+    # if i == 0:
+    #     label = r'$\alpha = $' + label
+    #     xpos = np.log10(Ns)[2 * (i + 1) - 4]
+    # ax.text(xpos, Ss[10 * (i + 1)], label, fontsize=15, c=c, backgroundcolor='white')
 
 #plt.plot(Ns, Ns, c='k', ls='dashed')
 #plt.arrow(11, 13, 5, -11, width = 0.1, head_width=0.5, facecolor='k', zorder=6)
 #plt.annotate(r'$\alpha$', xy=(np.log10(17), 1), xytext=(np.log10(11), 13), arrowprops=dict(arrowstyle="->", linestyle='--', linewidth=1.0))
 #plt.text(17, 1, r'$\alpha$')
 #plt.xscale('log')
-ax.set_xlim(1, 2)
-ax.set_ylim(0, 20)
+ax.set_xlim(np.log10(Ns[0]), np.log10(Ns[-1]))
+#ax.set_ylim(0, 20)
 ax.set_xlabel(r'$\log_{10}(N)$')
 #ax.set_ylabel(r'$\sum_j \, \, \, \tilde{J}_{ij}$')
 ax.set_ylabel(r'$\tilde N$')
 
 ax = axes[1]
 
-alphas = [0.5, 1.0, 1.5]
-Ns = np.arange(10, 100, 1)
+alphas = [0.5, 1, 1.5]
+Ns = np.arange(2, 20, 1)
 
-colors = iter(cmap(np.linspace(0.1, 0.9, len(alphas))))
+colors = iter(cmap(np.linspace(0.1, 0.9, 3)))
 for alpha in alphas:
     print(1/alpha)
     bs = []
     for N in Ns:
-        Jbase = interactions.powerlaw_pbc(N, alpha)
+        Jbase = interactions.powerlaw_pbc_2D(N, alpha)
         _, b = interactions.shift(Jbase, 0., return_shift=True)
         bs.append(b)
     bs = np.array(bs)
     
     c = next(colors)
     ax.plot(Ns, bs, label=alpha, c=c)
-    rs = np.arange(1, 1e8)
-    hline = -2 * np.sum((-1)**rs * rs**(-alpha))
-    ax.axhline(hline, c=c, ls='dotted')
 
 #plt.plot(Ns, Ns, c='k', ls='dashed')
 #plt.arrow(11, 13, 5, -11, width = 0.1, head_width=0.5, facecolor='k', zorder=6)
@@ -71,4 +68,4 @@ for alpha in alphas:
 ax.set_xlabel(r'$N$')
 ax.set_ylabel(r'$b$')
 ax.legend(title=r'$\alpha$', loc='lower right')
-fig.savefig('plots/bS.pdf', bbox_inches='tight', dpi=300)
+fig.savefig('plots/bS_2D.pdf', bbox_inches='tight', dpi=300)
